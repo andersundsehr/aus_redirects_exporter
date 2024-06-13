c44f1078364a6756d68ad3bb57dcd4ee5222f7b6
Lamm <s.lamm@andersundsehr.com>
2024-06-13 10:26:41 +0200

    WIP TYPO3 v12 compatibility

--- Classes/Command/WriterCommand.php
+++ Classes/Command/WriterCommand.php
@@ -12,12 +12,13 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class WriterCommand extends Command
 {
-    public function __construct()
+    public function __construct(protected readonly ExtensionConfiguration $extensionConfiguration)
     {
         parent::__construct('andersundsehr:redirects:writer');
     }
@@ -28,12 +29,16 @@ class WriterCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $extConf = $this->extensionConfiguration->get('redirects_expoter');
+        $directory = Environment::getProjectPath() . trim($extConf['directory'] . DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR);
+        $directoryNew = Environment::getProjectPath() . trim($extConf['directoryNew'] . DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR);
+        $directoryOld = Environment::getProjectPath() . trim($extConf['directoryOld'] . DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR);
+
         $redirects = GeneralUtility::makeInstance(RedirectRepository::class)->findForExport();
 
-        $directory = Environment::getProjectPath() . '/compose/nginx-assets';
-        GeneralUtility::mkdir_deep($directory . '/automated-redirects/');
-        GeneralUtility::mkdir_deep($directory . '/automated-redirects-new/');
-        array_map('unlink', array_filter((array)glob($directory . '/automated-redirects-new/*')));
+        GeneralUtility::mkdir_deep($directory);
+        GeneralUtility::mkdir_deep($directoryNew);
+        array_map('unlink', array_filter((array)glob($directoryNew . '*')));
         foreach ($redirects as $redirect) {
             if ($redirect['tx_ausredirects_exporter_resolved']) {
                 $stuff = json_decode($redirect['tx_ausredirects_exporter_resolved'], true, 512, JSON_THROW_ON_ERROR);
@@ -42,13 +47,13 @@ class WriterCommand extends Command
                 }
 
                 $filename = $redirect['source_host'] === '*' ? 'wildcard' : $redirect['source_host'];
-                file_put_contents($directory . '/automated-redirects-new/' . $filename . '.conf', $stuff['line'], FILE_APPEND);
+                file_put_contents($directoryNew . $filename . '.conf', $stuff['line'], FILE_APPEND);
             }
         }
         $output->writeln('exported', OutputInterface::VERBOSITY_VERBOSE);
         $output->writeln('try reload nginx...', OutputInterface::VERBOSITY_VERBOSE);
-        rename($directory . '/automated-redirects/', $directory . '/automated-redirects-old/');
-        rename($directory . '/automated-redirects-new/', $directory . '/automated-redirects/');
+        rename($directory, $directoryOld);
+        rename($directoryNew, $directory);
         $configTest = new Process(['sudo', 'nginx', '-t']);
         $resultCode = $configTest->run();
         $output->writeln(sprintf('config test %s', $resultCode ? 'Error' : 'Success'), OutputInterface::VERBOSITY_VERBOSE);
@@ -61,12 +66,12 @@ class WriterCommand extends Command
             $output->writeln('reloaded nginx', OutputInterface::VERBOSITY_VERBOSE);
             $output->write($reloadCommand->getOutput(), false, OutputInterface::VERBOSITY_VERBOSE);
             $output->write($reloadCommand->getErrorOutput());
-            GeneralUtility::rmdir($directory . '/automated-redirects-old/', true);
+            GeneralUtility::rmdir($directoryOld, true);
             return Command::SUCCESS;
         }
         $output->write('rollback configuration...');
-        rename($directory . '/automated-redirects/', $directory . '/automated-redirects-new/');
-        rename($directory . '/automated-redirects-old/', $directory . '/automated-redirects/');
+        rename($directory, $directoryNew);
+        rename($directoryOld, $directory);
         $output->write('rollback done');
         return Command::FAILURE;
     }
